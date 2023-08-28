fada35e4e0b17c12b785ef3c6220309301a51da3
BuildTools <unconfigured@null.spigotmc.org>
2023-08-28 23:19:02 +0300

    prjct1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, it's project 1");
         System.out.println("The weather is really hot today");
+        System.out.println("It was 35 degrees outside");
+
 
     }
 }
