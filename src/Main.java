95d499a6eeff6bcfc651b3ca64eb729d89c70e24
BuildTools <unconfigured@null.spigotmc.org>
2023-08-28 23:13:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi, it's project 1");
+        System.out.println("The weather is really hot today");
+
+    }
+}
